0638fdb7aa9196b4f6cfbd90938229668047537c
shivatyagi01 <shivaworx1@gmail.com>
2025-09-03 20:08:18 +0530

    Home page completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-$('#owl-carousel').owlCarousel({
-    loop: true,
-    margin: 30,
-    dots: true,
-    nav: true,
-    items: 2,
-})
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+});
